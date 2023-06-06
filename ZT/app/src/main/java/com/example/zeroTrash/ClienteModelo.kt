e5b3a30a5fb1d0f782d779a507f5583c9f68c42a
Dugeon <jose.b.neto@outlook.com>
2023-06-06 18:45:38 -0300

    Av2-DevMobile - ZeroTrash
    Envio do Projeto

---
+++ ZT/app/src/main/java/com/example/zeroTrash/ClienteModelo.kt
@@ -0,0 +1,9 @@
+package com.example.zeroTrash
+
+data class ClienteModelo(
+
+   var  empId: String? = null,
+   var NomeCliente: String? = null,
+   var EmailCliente: String? = null,
+   var SenhaCliente: String? = null
+)
